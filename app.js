c40bc04c49a2ef145615d7ec67433645c5ef51c8
Jules Ponsaillé <jules.ponsaille@isep.fr>
2020-01-07 18:16:41 +0100

    better production settings

--- app.js
+++ app.js
@@ -10,7 +10,13 @@ var calendarRouter = require('./routes/calendar');
 
 var app = express();
 
-app.use(logger('dev'));
+if(process.env.NODE_ENV === "production") {
+  app.use(logger('combined'));
+} else {
+  app.use(logger('dev'));
+}
+
+app.set('trust proxy', true);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
